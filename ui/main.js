ef1e2d5ae80128360f7d949ac39aea47de4a4bd3
RadhaRamani <rrvedala@gmail.com>
2016-09-28 19:12:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="WOW!!!";
 var img=document.getElementById("madi");
+
 img.onClick=function(){
     
     img.style.marginLeft= '100px';
