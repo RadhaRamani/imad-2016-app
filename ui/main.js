704103c2e24d0ffa5baaa45ca39ec86e9f90d98d
RadhaRamani <rrvedala@gmail.com>
2016-09-28 20:08:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="WOW!!!";
 var img=document.getElementById("madi");
@@ -11,4 +11,24 @@ function moveRight()
 img.onClick=function(){
     
    var interval=setInterval(moveRight,50);
+};*/
+var counter=0;
+var button=document.getElementById("counter");
+button.onClick =function()
+{
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+      
+      if(request.readyState==XMLHttpRequest.DONE)  
+      {
+          if(request.status==200)
+          {
+              var counter=request.responseText;
+              var span=document.getElementById("count");
+                span.innerHTML=counter.toString();
+          }
+      }
+    };
+    request.open('GET','http://radharamani.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
