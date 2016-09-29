d54fcef142ca5c94b29875c0948dcc5dface4c4e
RadhaRamani <rrvedala@gmail.com>
2016-09-29 14:46:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,17 +74,18 @@ app.get('/submit-name',function(req,res){
     res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName',function(req,res){
+/*app.get('/:articleName',function(req,res){
    
      var articleName=req.params.articleName;
      //res.send(createtemplate(articles[articleName]));
-});
+});*/
 
 
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
