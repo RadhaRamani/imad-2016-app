7857ca1ec627362fd79438af40278e3f31e4ca56
RadhaRamani <rrvedala@gmail.com>
2016-09-29 13:26:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,6 +69,14 @@ app.get('/:articleName',function(req,res){
      res.send(createtemplate(articles[articleName]));
 });
 
+var names=[];
+app.get('/submit-name/:name',function(req,res){
+    var name=req.params.name;
+    names.push(name);
+    
+    res.send(JSON.stringify(names));
+});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
@@ -80,13 +88,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-var names=[];
-app.get('/submit-name/:name',function(req,res){
-    var name=req.params.name;
-    names.push(name);
-    
-    res.send(JSON.stringify(names));
-});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
