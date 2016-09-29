83c15223a52edf2e1b2a51f30586fb6c92e43694
RadhaRamani <rrvedala@gmail.com>
2016-09-29 15:59:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.get('/submit-name',function(req,res){
 app.get('/:articleName',function(req,res){
    
      var articleName=req.params.articleName;
-     //res.send(createtemplate(articles[articleName]));
+     res.send(createtemplate(articles[articleName]));
 });
 
 
